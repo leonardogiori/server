4b2b46602561be53da61b90af8d0ad5c4bace3ca
Leonardo Giori <98846766+leonardogiori@users.noreply.github.com>
2022-10-05 15:35:17 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,77 +1,75 @@
+#!/bin/bash
 
+###############################################################################################################################
+# Intall server - Ubuntu 20.04                                                                                                #
+# Version - 0.1                                                                                                               #
+###############################################################################################################################
+
+# Colors
+RED=$'\e[0;31m'
+GREEN=$'\e[0;32m'
+BLUE=$'\e[0;34m'
+B=$'\n\e[1;33m'
+C=$'\e[1;30m'
+N=$'\e[0m'
+
+# Vars
 DOMAIN='giori.com.br'
 PHP_VERSION='8.1'
 NGINX_DEF='/etc/nginx/sites-available/default'
+NGINX_GONF='/etc/nginx/nginx.conf'
 LOG_FORMAT='$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$http_x_forwarded_for"'
 MONIT_CONF='/etc/monit/monitrc'
+
+
+
+
+# Clear Terminal
+sudo clear
+
+
+
+# NGINX #######################################################################################################################
+###############################################################################################################################
+
+# Install NGINX
+echo -e "${B}Install Nginx ${N}"
+sudo apt install nginx -y
+
+# Install NGINX Secure
+echo -e "${B}Install NGINX Secure ${N}"
+sudo ufw allow "Nginx HTTP"
+#sudo ufw status
 
 
 # Configure NGINX
 echo -e "${B}Configure NGINX ${N}"
 
-sudo chmod 777 -R $NGINX_DEF
-echo 'user www-data;' > $NGINX_DEF
-echo 'worker_processes auto;' >> $NGINX_DEF #1;' >> $NGINX_DEF
-echo 'error_log /var/log/nginx/error.log warn;' >> $NGINX_DEF
-echo 'pid       /var/run/nginx.pid;' >> $NGINX_DEF
-echo 'events {' >> $NGINX_DEF
-echo '    worker_connections 1024;' >> $NGINX_DEF
-echo '}' >> $NGINX_DEF
-echo 'http {' >> $NGINX_DEF
-echo '    include /etc/nginx/mime.types;' >> $NGINX_DEF
-echo '    default_type application/octet-stream;' >> $NGINX_DEF
-echo "    log_format main '${LOG_FORMAT}';" >> $NGINX_DEF
-echo '    access_log /var/log/nginx/access.log main;' >> $NGINX_DEF
-echo '    sendfile on;' >> $NGINX_DEF
-echo '    keepalive_timeout 65;' >> $NGINX_DEF
-
+sudo mv "${NGINX_CONF}" "${NGINX_CONF}.old"
+sudo mv "${NGINX_DEF}" "${NGINX_DEF}.old"
 
 echo '    server {' >> $NGINX_DEF
-
-
 echo '        listen                  [::]:443 ssl ipv6only=on;' >> $NGINX_DEF
 echo '        listen                  443 ssl;' >> $NGINX_DEF
 echo "        ssl_certificate         /etc/letsencrypt/live/${DOMAIN}/fullchain.pem;" >> $NGINX_DEF
 echo "        ssl_certificate_key     /etc/letsencrypt/live/${DOMAIN}/privkey.pem;" >> $NGINX_DEF
 echo '        include                 /etc/letsencrypt/options-ssl-nginx.conf;' >> $NGINX_DEF
 echo '        ssl_dhparam             /etc/letsencrypt/ssl-dhparams.pem;' >> $NGINX_DEF
-
-
-echo '        root /var/www/html/;' >> $NGINX_DEF
-echo '        index index.php;' >> $NGINX_DEF
-
-echo '        server_name ~^(?<subdomain>\w+)\.(?<domain>.+)$;' >> $NGINX_DEF
-
-echo '        client_max_body_size 32M;' >> $NGINX_DEF
-echo '        autoindex off;' >> $NGINX_DEF
-echo '        allow all;' >> $NGINX_DEF
-
-echo '        location ~ ^/(img|css|js|video)/ {' >> $NGINX_DEF        
-echo '            try_files /$subdomain/$domain$uri /$subdomain/default$uri /$uri;' >> $NGINX_DEF
-echo '        }' >> $NGINX_DEF
-
-echo '        location / {' >> $NGINX_DEF
-echo '            rewrite ^/([a-zA-Z0-9\.\-_\~/]+)$ /index.php?_uri=$1 last;' >> $NGINX_DEF
-echo '            location = /index.php {' >> $NGINX_DEF
-echo '                  include snippets/fastcgi-php.conf;' >> $NGINX_DEF
-echo "                  fastcgi_pass unix:/run/php/php${PHP_VERSION}-fpm.sock;" >> $NGINX_DEF
-echo '            }' >> $NGINX_DEF
-echo '            allow all; ' >> $NGINX_DEF
-echo '        }' >> $NGINX_DEF
-
-echo '    }' >> $NGINX_DEF
-
-
-
 echo '    server {' >> $NGINX_DEF
-echo '      return 301 https://$host$request_uri;' >> $NGINX_DEF
+echo '        return 301 https://$host$request_uri;' >> $NGINX_DEF
 echo '    }' >> $NGINX_DEF
 
 
 
-echo '}' >> $NGINX_DEF
+
+
 sudo chmod 755 -R $NGINX_DEF
 
 # Restart NGINX
 echo -e "${B}Restart NGINX ${N}"
 sudo systemctl reload nginx
+
+
+
+
+fi
