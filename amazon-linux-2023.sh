7fa304ec080d1600882de78ebc0733d3e9cade50
Leonardo Giori <98846766+leonardogiori@users.noreply.github.com>
2024-03-16 09:26:15 -0300

    Update amazon-linux-2023.sh

--- amazon-linux-2023.sh
+++ amazon-linux-2023.sh
@@ -1,3 +1,4 @@
+# Configure nGinx
 nginx_conf='
 user nginx;
 worker_processes 1;
@@ -19,6 +20,9 @@ http {
         root /var/www/html;
         autoindex off;
         allow all;
+        location ~ ^/(.*?)/acme-challenge/(.*?) {
+            try_files $uri /$uri $uri/ =404;
+        }
         location ~ \.txt$ {
             try_files $uri /$uri $uri/ =404;
         }
@@ -29,16 +33,16 @@ http {
     server {
         listen                  443 ssl default_server;
         listen                  [::]:443 ssl default_server;
-        ssl_certificate         /etc/ssl/certs/localhost.crt;
-        ssl_certificate_key     /etc/ssl/certs/localhost.key;
-        ssl_protocols           SSLv3 TLSv1 TLSv1.1 TLSv1.2;
-        ssl_ciphers             HIGH:!aNULL:!MD5;
         client_max_body_size 32M;
         server_name ~^(?<subdomain>\w+)\.(?<domain>.+)$;
         root /var/www/;
         autoindex off;
         index index.php;
         allow all;
+        ssl_certificate         /etc/letsencrypt/live/giori.com.br/fullchain.pem;
+        ssl_certificate_key     /etc/letsencrypt/live/giori.com.br/privkey.pem;
+        ssl_protocols           SSLv3 TLSv1 TLSv1.1 TLSv1.2;
+        ssl_ciphers             HIGH:!aNULL:!MD5;
         location ~ ^/asset/(?:([^\/]+)/)?(.*)$ {
             try_files /data/storage/$1/$2 /data/static/$1/$2 /html/$domain/$subdomain/asset/$1/$2 /core/$1/asset/$2 /data/static/$domain/$subdomain/asset/$1/$2 /static/$1/$2;
         }
@@ -46,11 +50,10 @@ http {
             rewrite ^/([a-zA-Z0-9\.\-_\~@/]+)$ /index.php?_uri=$1 last;
             location = /index.php {
                 fastcgi_pass unix:/var/run/php-fpm/www.sock;
-                #fastcgi_index index.php;
                 fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
                 include fastcgi_params;
             }  
-            allow all;   
+            allow all;
         }
     }
 }
